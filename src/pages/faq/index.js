64d929c9ce7a70b31be13e6303deeed4547cdec7
CaveiraRussa <Luiz.m.santiago@hotmail.com>
2021-05-23 22:44:08 -0300

    Refactor FAQ page

--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -13,53 +13,67 @@ import {
 } from "react-native";
 import styles from "./styles";
 import logoImg from "../../assets/faq.png";
+import addImg from "../../assets/add.png";
 
 export default function faq() {
   const [faq, setFaq] = useState([]);
-
+  const [state, setState] = useState([false]);
   const navigation = useNavigation();
 
   function navigateToPerfil() {
     navigation.navigate("perfil");
   }
 
+  function toggleStatus() {
+    setState(!state);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.InnerContainer}>
         <StatusBar hidden />
-        <TouchableOpacity
-          style={styles.detailsButton}
-          onPress={navigateToPerfil}
-        >
-          <Text style={styles.detailsButtonText}>Voltar</Text>
-          <Feather name="arrow-right" size={16} color="#FFF" />
-        </TouchableOpacity>
-        <Image style={styles.imagem} source={logoImg} />
-        <Text style={styles.tituloPerfil}>Perguntas Frequentes</Text>
-        <View style={styles.corpoPerguntas}>
-          <Text style={styles.title}>
-            As vacinas atuais protegem contra as novas variantes de coronavírus,
-            como as linhagens de Manaus e do Reino Unido?
-          </Text>
-          <Button
-            title="Leia Mais +"
-            onPress={() => Alert.alert("Simple Button pressed")}
-          />
-          <Text style={styles.title}>
-            A vacina contra a Covid-19 já está disponível? Já posso me vacinar?{" "}
-          </Text>
-          <Button
-            title="Leia Mais +"
-            onPress={() => Alert.alert("Simple Button pressed")}
-          />
-          <Text style={styles.title}>
-            Apenas as pessoas dos grupos prioritários serão imunizadas?{" "}
-          </Text>
-          <Button
-            title="Leia Mais +"
-            onPress={() => Alert.alert("Simple Button pressed")}
-          />
+        <View style={styles.header}>
+          <TouchableOpacity
+            style={styles.detailsButton}
+            onPress={navigateToPerfil}
+          >
+            <Text style={styles.detailsButtonText}>Voltar</Text>
+            <Feather name="arrow-right" size={16} color="#6C63FF" />
+          </TouchableOpacity>
+          <Image style={styles.imagem} source={logoImg} />
+          <Text style={styles.tituloPerfil}>Perguntas Frequentes</Text>
         </View>
+        <View style={styles.Questionsheader}>
+          <View style={styles.corpoPerguntas}>
+            <Text style={styles.title} onPress={toggleStatus}>
+              As vacinas atuais protegem contra as novas variantes de
+              coronavírus, como as linhagens de Manaus e do Reino Unido?
+            </Text>
+            <View>
+              {!state ? <Text style={styles.answer}>sla irmão </Text> : null}
+            </View>
+          </View>
+          <View style={styles.corpoPerguntas}>
+            <Text style={styles.title} onPress={toggleStatus}>
+              A vacina contra a Covid-19 já está disponível? Já posso me
+              vacinar?{" "}
+            </Text>
+            <View>
+              {!state ? <Text style={styles.answer}>Sim</Text> : null}
+            </View>
+          </View>
+          <View style={styles.corpoPerguntas}>
+            <Text style={styles.title} onPress={toggleStatus}>
+              Apenas as pessoas dos grupos prioritários serão imunizadas?{" "}
+            </Text>
+            <View>
+              {!state ? (
+                <Text style={styles.answer}>Talvez nem eles</Text>
+              ) : null}
+            </View>
+          </View>
+        </View>
+        <Image style={styles.add} source={addImg} />
       </View>
     </View>
   );
--- src/pages/faq/styles.js
+++ src/pages/faq/styles.js
@@ -1,77 +1,99 @@
-import {StyleSheet} from 'react-native';
-import Constants from 'expo-constants';
+import { StyleSheet } from "react-native";
+import Constants from "expo-constants";
 
 export default StyleSheet.create({
-
-    container:{
-        flex: 1,
-        backgroundColor: '#6C63FF',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding:20,
-    },
-    InnerContainer: {
-        flex: 1,
-        backgroundColor: "#FFF",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: 40,
-        borderRadius: 16,
-      },
-    imagem:{
-        bottom: 80,
-        marginRight: 5,
-        resizeMode: "contain",
-    },
-    tituloPerfil:{
-        bottom: 40,
-        width:'100%',
-        height:40,
-        flexDirection:'row',
-        borderRadius:10,
-        borderWidth: 1,
-        backgroundColor:'#FFF',
-        fontSize:24,
-        color:'#6C63FF',
-        fontWeight:'bold',
-        justifyContent:'center',
-        alignItems:'center',
-    },
-    faq:{
-        color:'#FFF',
-        fontWeight:'bold',
-        justifyContent:'center',
-        alignItems:'center',
-        marginTop:50,
-
-    },
-    botão:{
-        width: '50%',
-        height:40,
-    },
-    scrollView: {
-        backgroundColor: '#FFF',
-        marginHorizontal: 20,
-      },
-      title:{
-        borderRadius:8,
-        backgroundColor:'#FFF',
-        marginTop:10,
-
-      },
-      detailsButton:{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        alignItems:'center',
-        marginLeft:260,
-        marginTop:-150,
-       
-    },
-    detailsButtonText:{
-        color:'#FFF',
-        fontSize:15,
-        fontWeight:'bold',
-        
-        
-    },
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#6C63FF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  InnerContainer: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 16,
+    width:"95%",
+    maxHeight:"90%",
+    bottom:30,
+  },
+  header:{
+    position: 'absolute',
+    top:160,
+  },
+  imagem: {
+    marginRight: 5,
+    resizeMode: "contain",
+    bottom:10
+  },
+  tituloPerfil: {
+    fontSize: 18,
+    color: "#6C63FF",
+    fontWeight: "bold",
+    borderWidth: 1,
+    borderColor: "#6C63FF",
+    borderRadius: 15,
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    top:10,
+  },
+  faq: {
+    color: "#252071",
+    fontWeight: "bold",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 50,
+  },
+  botão: {
+    width: "50%",
+    height: 40,
+  },
+  scrollView: {
+    backgroundColor: "#FFF",
+    marginHorizontal: 20,
+  },
+  Questionsheader:{
+    position: 'absolute',
+    top:280,
+  },
+  corpoPerguntas: {
+    marginBottom:5,
+  },
+  title: {
+    color: "#252071",
+    width:350,
+    borderRadius: 5,
+    backgroundColor: "#C4C4C4",
+    padding:8,
+  },
+  answer: {
+    color: "#08B1F9",
+    width:320,
+    borderRadius: 5,
+    backgroundColor: "#DADADA",
+    padding:8,
+    left:15
+  },
+  detailsButton: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginLeft: 260,
+    marginTop: -150,
+  },
+  detailsButtonText: {
+    color: "#FFF",
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  add:{
+    position: 'absolute',
+    bottom:30,
+    right:30,
+    height:60,
+    width:50,
+  }
+});
