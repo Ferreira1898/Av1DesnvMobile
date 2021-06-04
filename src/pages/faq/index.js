984250c91b259e9eebd5c9bca9eb9a68b4c52278
CaveiraRussa <Luiz.m.santiago@hotmail.com>
2021-06-03 22:20:34 -0300

    Creating mocked questions for faq

--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -20,15 +20,13 @@ import { detailsOptions } from '../../components/detailOptions/detailOptions.js'
 export function Faq() {
   const [faq, setFaq] = useState([]);
   const [state, setState] = useState([false]);
+  const [state1, setState1] = useState([false]);
+  const [state2, setState2] = useState([false]);
+
   const navigation = useNavigation();
-  //const [optionView, setOptionView] = useState<string>("[]");
 
   function navigateToPerfil() {
     navigation.navigate('perfil');
-  }
-
-  function toggleStatus() {
-    setState(!state);
   }
 
   return (
@@ -46,13 +44,43 @@ export function Faq() {
           <Image style={styles.imagem} source={logoImg} />
           <Text style={styles.tituloPerfil}>Perguntas Frequentes</Text>
         </View>
-        {/*
         <View style={styles.Questionsheader}>
-          {faqModel.map((value) => {
-            return(<detailsOptions />)
-          })}
+          <View style={styles.corpoPerguntas}>
+            <Text style={styles.title} onPress={() => setState(!state)}>
+              As vacinas atuais protegem contra as novas variantes de
+              coronavírus, como as linhagens de Manaus e do Reino Unido?
+            </Text>
+            <View>
+              {!state ? <Text style={styles.answer}>Sim </Text> : null}
+            </View>
+          </View>
+          <View style={styles.corpoPerguntas}>
+            <Text style={styles.title} onPress={() => setState1(!state1)}>
+              A vacina contra a Covid-19 já está disponível? Já posso me
+              vacinar?
+            </Text>
+            <View>
+              {!state1 ? (
+                <Text style={styles.answer}>
+                  Sim, mas devera esperar que chega ao seu momento de se vacinar
+                </Text>
+              ) : null}
+            </View>
+          </View>
+          <View style={styles.corpoPerguntas}>
+            <Text style={styles.title} onPress={() => setState2(!state2)}>
+              Apenas as pessoas dos grupos prioritários serão imunizadas?
+            </Text>
+            <View>
+              {!state2 ? (
+                <Text style={styles.answer}>
+                  Não, eles irão receber primeiro a vacina, e após isso será
+                  iniciada a vacinação em outras pessoas
+                </Text>
+              ) : null}
+            </View>
+          </View>
         </View>
-        */}
         <Image style={styles.add} source={addImg} />
       </View>
     </View>
